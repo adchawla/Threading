d4acb13c9737dc3d32509e52f30f162a7ac42705
Amandeep Chawla <adchawla@gmail.com>
2016-02-11 05:16:59 +0530

    Creating a thread

--- Threading/main.cpp
+++ Threading/main.cpp
@@ -1,15 +1,13 @@
-//
-//  main.cpp
-//  Threading
-//
-//  Created by Amandeep Chawla on 11/02/16.
-//  Copyright © 2016 Amandeep Chawla. All rights reserved.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <thread>
+using namespace std;
+void threadFunc()
+{
+    cout << "Welcome to Multithreading" << endl;
+    
+}
+int main()
+{
+    //pass a function to thread
+    thread funcTest1(threadFunc);
 }
