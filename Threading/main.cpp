b305be04fd31b461a8805e59a09a1907f643a93a
Amandeep Chawla <adchawla@gmail.com>
2016-02-11 05:29:58 +0530

    Using Public Function of the object to initialize thread

--- Threading/main.cpp
+++ Threading/main.cpp
@@ -9,13 +9,18 @@ public:
     {
         cout << "This is my function object" << endl;
     }
+    
+    void publicFunction()
+    {
+        cout << "public function of myFunctor class is called" << endl;
+    }
 };
 
 int main()
 {
     //pass a function to thread
     myFunctor myFunc;
-    thread funcTest1(myFunc);
+    thread funcTest1(&myFunctor::publicFunction, myFunc);
     if ( funcTest1.joinable())
         funcTest1.join();
 }
