5a40abd6472cdcd973192dd798a4a85c45297393
Amandeep Chawla <adchawla@gmail.com>
2016-02-11 07:45:26 +0530

    Copying Two Files one after another

--- Threading/main.cpp
+++ Threading/main.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <thread>
+#include <fstream>
+
 #include "LogTiming.h"
 using namespace std;
 
-class myFunctor
-{
-public:
-    void operator()()
-    {
-        cout << "This is my function object" << endl;
-    }
+void CopyFile(const char * source, const char * destination, char * bufferToUse, size_t bufferSize ) {
     
-    void publicFunction()
-    {
-        cout << "public function of myFunctor class is called" << endl;
-        sleep(2);
+    ifstream sourceFile( source, ios::in | ios::binary );
+    ofstream destinationFile( destination, ios::out | ios::binary );
+    
+    while ( sourceFile.is_open() ) {
+        sourceFile.read( bufferToUse, bufferSize );
+        size_t bytesRead = sourceFile.gcount();
+        if ( bytesRead > 0 ) {
+            destinationFile.write( bufferToUse, bytesRead );
+        } else {
+            sourceFile.close();
+            destinationFile.close();
+        }
+        if ( sourceFile.bad() || sourceFile.eof() || sourceFile.fail() ) {
+            sourceFile.close();
+            destinationFile.close();
+        }
     }
-};
+}
 
 int main()
 {
     LogTiming timer = LogTiming("main");
     //pass a function to thread
-    myFunctor myFunc;
-    thread funcTest1(&myFunctor::publicFunction, myFunc);
-    if ( funcTest1.joinable())
-        funcTest1.join();
+    size_t bufferSize = 20 * 1024 * 1024;
+    char * buffer = new char[bufferSize];
+    CopyFile( "/Users/amandeep/temp/1.mp4", "/Users/amandeep/temp/dest/1.mp4", buffer, bufferSize);
+    CopyFile( "/Users/amandeep/temp/2.mp4", "/Users/amandeep/temp/dest/2.mp4", buffer, bufferSize);
 }
