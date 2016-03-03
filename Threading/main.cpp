2efc77140c7b048c7546c58e3132e8d9482a9d85
Amandeep Chawla <adchawla@gmail.com>
2016-03-03 10:52:10 +0530

    Producer Consumer Problem without communication

--- Threading/main.cpp
+++ Threading/main.cpp
@@ -7,47 +7,51 @@
 #include "LogTiming.h"
 
 using namespace std;
-mutex mx;
-size_t throughput = 0;
 
-void CopyFile(const char * source, const char * destination, size_t bufferSize ) {
-    
-    char * buffer = new char[ bufferSize ];
- 
-    ifstream sourceFile( source, ios::in | ios::binary );
-    ofstream destinationFile( destination, ios::out | ios::binary );
-    
-    while ( sourceFile.is_open() ) {
-        sourceFile.read( buffer, bufferSize );
-        size_t bytesRead = sourceFile.gcount();
-        if ( bytesRead > 0 ) {
-            destinationFile.write( buffer, bytesRead );
-            AutoReleaseLock lock(mx);
-            throughput += bytesRead;
-        } else {
-            sourceFile.close();
-            destinationFile.close();
-            break;
-        }
-        if ( sourceFile.bad() || sourceFile.eof() || sourceFile.fail() ) {
-            sourceFile.close();
-            destinationFile.close();
-            break;
-        }
-    }
+#define N 100
+char buffer[ N ];
+size_t countOfItmes = 0;
+mutex mx;
+
+#include <stdlib.h>
+
+void Producer() {
+	while ( true ) {
+		char item = rand() % 26 + 'A';
+		{
+			AutoReleaseLock lock( mx );
+			if ( countOfItmes == N - 1 ) {
+				std::cerr << "Could not Push anything \n";
+				continue;
+			}
+			buffer[ countOfItmes++ ] = item;
+			std::cerr << "Pushed " << item << " at " << countOfItmes - 1 << " location\n";
+		}
+		std::this_thread::sleep_for( std::chrono::milliseconds( rand() % 1000 ) );
+	}
+}
+
+void Consumer() {
+	while ( true ) {
+		{
+			AutoReleaseLock lock( mx );
+			if ( countOfItmes == 0 ) {
+				std::cerr << "Could not Pull anything \n";
+				continue;
+			}
+			char item = buffer[ --countOfItmes ];
+			std::cerr << "Pulled " << item << " from " << countOfItmes << " location\n";
+		}
+		std::this_thread::sleep_for( std::chrono::milliseconds( rand() % 1000 ) );
+	}
 }
 
 int main()
 {
-    LogTiming timer = LogTiming("main");
-    //pass a function to thread
-    size_t bufferSize = 20 * 1024 * 1024;
-    throughput = 0;
-    thread t1(CopyFile, "/Users/amandeep/temp/1.mp4", "/Users/amandeep/temp/dest/1.mp4", bufferSize);
-    thread t2(CopyFile, "/Users/amandeep/temp/2.mp4", "/Users/amandeep/temp/dest/2.mp4", bufferSize);
+    thread t1(Producer);
+	thread t2( Consumer);
     if ( t1.joinable() )
         t1.join();
     if ( t2.joinable() )
         t2.join();
-    std::cout<<"Total bytes transferred: "<<throughput<<"\n";
 }
